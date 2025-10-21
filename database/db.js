29a0b3286a7dbe7e95e68308662c217670e58e2e
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-10-21 09:37:52 +0300

    Upload Project

---
+++ database/db.js
@@ -0,0 +1,23 @@
+import pkg from "pg";
+import { config } from 'dotenv';
+config({ path: '../config/config.env' });
+
+const { Client } = pkg;
+
+const database = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "mern_ecommerce_store",
+  password: "Admin",
+  port: 5432,
+});
+
+try {
+  await database.connect();
+  console.log("Connected to the database successfully");
+} catch (error) {
+  console.error("Database connection failed:", error);
+  process.exit(1);
+}
+
+export default database;
